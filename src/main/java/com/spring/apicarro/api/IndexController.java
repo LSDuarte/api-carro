ec7a392cc43f27113fbe7dafd157e3953e667a86
Larissa Duarte <42499@pjmt.local>
2021-03-09 09:30:41 -0400

    aprendendo passar parametros

--- src/main/java/com/spring/apicarro/api/IndexController.java
+++ src/main/java/com/spring/apicarro/api/IndexController.java
@@ -1,6 +1,7 @@
 package com.spring.apicarro.api;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,10 +15,26 @@ public class IndexController {
 		return "Get Spring Boot";
 	}
 
-	// aprendendo a passar parametro
-	@GetMapping("/login")
-	public String login(@RequestParam("user") String user, @RequestParam("pass") String pass) {
-		return "Login: " + user + ", Pass: " + pass;
+	// aprendendo a passar parametro [http://localhost:8080/loginParam?user=larissa&pass=123]
+	@GetMapping("/loginParam")
+	public String loginParam(@RequestParam("user") String user, @RequestParam("pass") String pass) {
+		return "Login: " + user + ", Senha: " + pass;
+	}
+	
+	// aprendendo a passar variaveis [http://localhost:8080/loginVariable/larissa/senha/123]
+	@GetMapping("/loginVariable/{user}/senha/{pass}")
+	public String loginVariable(@PathVariable("user") String user, @PathVariable("pass") String pass) {
+		return "Login: " + user + ", Senha: " + pass;
+	}
+	
+	@GetMapping("/carros/{id}")
+	public String getCarroById(@PathVariable("id") Long id) {
+		return "Carro by id " + id;
+	}
+	
+	@GetMapping("/carros/tipo/{tipo}")
+	public String getCarroByTipo(@PathVariable("tipo") String tipo) {
+		return "Lista de Carros " + tipo;
 	}
 
 }
