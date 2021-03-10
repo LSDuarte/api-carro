d053dbc48a14aa51f301d5c5091c130a2c99a735
Larissa Duarte <42499@pjmt.local>
2021-03-10 08:20:05 -0400

    aprendendo a passar parametros via POST com Postman

--- src/main/java/com/spring/apicarro/api/IndexController.java
+++ src/main/java/com/spring/apicarro/api/IndexController.java
@@ -2,6 +2,7 @@ package com.spring.apicarro.api;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +15,13 @@ public class IndexController {
 	public String get() {
 		return "Get Spring Boot";
 	}
-
+	
+	// aprendendo a passar parametro sem aparecer os parametros na URL, com POST [http://localhost:8080/login] passando User e Pass via Postman
+	@PostMapping("/login")
+	public String login(@RequestParam("user") String user, @RequestParam("pass") String pass) {
+		return "Login: " + user + ", Senha: " + pass;
+	}
+	
 	// aprendendo a passar parametro [http://localhost:8080/loginParam?user=larissa&pass=123]
 	@GetMapping("/loginParam")
 	public String loginParam(@RequestParam("user") String user, @RequestParam("pass") String pass) {
@@ -27,6 +34,7 @@ public class IndexController {
 		return "Login: " + user + ", Senha: " + pass;
 	}
 	
+	// lidando com rotas
 	@GetMapping("/carros/{id}")
 	public String getCarroById(@PathVariable("id") Long id) {
 		return "Carro by id " + id;
