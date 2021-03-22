69a6087c0ebd2d22c9fa0bdec6003b23dc3c8e71
Larissa Duarte <42499@pjmt.local>
2021-03-22 10:52:25 -0400

    Iniciando Teste com spring boot

--- src/test/java/com/spring/apicarro/ApiCarroApplicationTests.java
+++ src/test/java/com/spring/apicarro/ApiCarroApplicationTests.java
@@ -1,13 +1,25 @@
 package com.spring.apicarro;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.spring.apicarro.domain.Carro;
+import com.spring.apicarro.domain.CarroService;
 
 @SpringBootTest
 class ApiCarroApplicationTests {
 
+	@Autowired
+	private CarroService service;
+	
 	@Test
-	void contextLoads() {
+	public void test1() {
+		Carro carro = new Carro();
+		carro.setNome("Ferrari");
+		carro.setTipo("Esportivos");
+		
+		service.insert(carro);
 	}
 
 }
